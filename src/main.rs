193a066949d1684be5052ef3271dd375cacf8c8d
Mateo C Lopez <mlo5976@gmail.com>
2021-01-30 13:40:48 -0500

    First Website Attempt

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(fs::Files::new("/", "./static")
+        .index_file("index.html")
+        .show_files_listing())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
